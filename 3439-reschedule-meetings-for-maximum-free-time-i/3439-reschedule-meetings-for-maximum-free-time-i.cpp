acd8db968d8c969c351e6b2b3930d1bca6070762
Sameer Bhandari <129886570+Sameer5718@users.noreply.github.com>
2025-07-09 21:57:14 +0530

    Time: 72 ms (11.9%), Space: 171.4 MB (5.24%) - LeetHub

--- 3439-reschedule-meetings-for-maximum-free-time-i/3439-reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3439-reschedule-meetings-for-maximum-free-time-i/3439-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -14,31 +14,47 @@ public:
             ev.push_back({startTime[i], endTime[i]});
         }
         vector<int>gaps;
-        if(ev[0][0]!=0){
-            gaps.push_back(ev[0][0]-0);
-        }
-        else gaps.push_back(0);
+       // if(ev[0][0]!=0){
+            gaps.push_back(ev[0][0]);
+       // }
+       // else gaps.push_back(0);
         for(int i = 1; i<ev.size(); i++){
-            if(ev[i][0]>ev[i-1][1])
-                gaps.push_back(ev[i][0]-ev[i-1][1]);
+            gaps.push_back(ev[i][0]-ev[i-1][1]);
         }
         if(ev[ev.size()-1][1]<eventTime)gaps.push_back(eventTime-ev[ev.size()-1][1]);
+        
         // run a k+1 size window over gaps
         int m = INT_MIN;
-        int i = 0; 
-        while(i<gaps.size()){
-            int count = 0;
-            int sum = 0;
-            int j = i;
-            while(count<k+1 && j<gaps.size()){
-                sum+=gaps[j];
-                count++;
-                j++;
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        int n = gaps.size();
+        int cursum = 0;
+        while(j<n){
+            cursum+=gaps[j];
+            if(i<n && j-i+1 > k+1){
+                cursum-=gaps[i];
+                i++;
             }
-            //i%=k;
-            i++;
-            m = max(m, sum);
-        }
-        return m==INT_MIN ? 0 : m;
+            sum = max(sum, cursum);
+            j++;
+        } 
+        return sum;
+        // while(i<gaps.size()){
+        //     int count = 0;
+        //     int sum = 0;
+        //     int j = i;
+        //     int ans = 0;
+        //     while(count<k+1 && j<gaps.size()){
+        //         sum+=gaps[j];
+        //         ans=max(ans,sum);
+        //         count++;
+        //         j++;
+        //     }
+        //     //i%=k;
+        //     i++;
+        //     m = max(m, ans);
+        // }
+        // return m==INT_MIN ? 0 : m;
     }
 };
