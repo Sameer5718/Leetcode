ba802e8496eb2d41052f41c3cfa46c9f3adde6d7
Sameer Bhandari <129886570+Sameer5718@users.noreply.github.com>
2025-11-25 12:57:02 +0530

    Time: 7 ms (20.5%), Space: 9.8 MB (47.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // create 2 maps to store the freq of each char and check 
+        if(s.size() != t.size())return 0;
         map<char,int>m1;
         map<char,int>m2;
-        int n = s.size();
-        int m = t.size();
-        if(n!=m)return 0;
-        for(int i = 0; i<n; i++){
+        for(int i = 0; i<s.size(); i++){
             m1[s[i]]++;
         }
-        for(int i = 0; i < m; i++){
+        for(int i = 0; i<t.size(); i++){
             m2[t[i]]++;
         }
         auto it = m1.begin();
-        while(it!=m1.end()){
-            if(m2.find(it->first) != m2.end() && m2[it->first] != it->second){
-                return 0;
-            }
-            else if(m2.find(it->first) == m2.end())return 0;
+        auto it2= m2.begin();
+        while(it!=m1.end() || it2!=m2.end()){
+            if(it->first != it2->first || it->second != it2->second)return 0;
             it++;
+            it2++;
         }
         return 1;
     }
